5b0037b1289a3fe6a650ec2ad36d4d05a67fc38a
Divine Olokor <divee789@gmail.com>
2020-03-30 09:32:50 +0100

    Iniial commit

---
+++ assets/js/init.js
@@ -0,0 +1,101 @@
+
+
+jQuery(function ($) {
+console.log('Initiaizing moddal')
+    var paymentRequest = async function () {
+
+        var $form = $('form#payment-form, form#order_review'),
+            korapay_referenceKey = $form.find('input.korapay_referenceKey')
+        
+        //Empty the reference Input
+        korapay_referenceKey.val('');
+
+        //Make sure that the amount is a number
+        const total = Number(korapay_params.amount)
+
+
+        var errorCallback = function (response) {
+            let message = 'failure'
+
+            if (response.reference === null) {
+                $form.append('<input type="hidden" class="korapay_referenceKey" name="korapay_referenceKey" value="' + korapay_params.orderId + '"/>');
+                $form.append('<input type="hidden" class="modal_failure" name="modal_failure" value="' + message + '"/>');
+                $form.submit();
+                return
+            }
+            
+            //Append the reference and error to the form
+            $form.append('<input type="hidden" class="korapay_referenceKey" name="korapay_referenceKey" value="' + response.reference + '"/>');
+            $form.append('<input type="hidden" class="trans_failure" name="trans_failure" value="' + message + '"/>');
+
+             //Subnit the form to the payment gateway
+            $form.submit();
+
+         //Create a backdrop
+            $('body').block({
+                message: null,
+                overlayCSS: {
+                    background: '#fff',
+                    opacity: 0.6
+                },
+                css: {
+                    cursor: "wait"
+                }
+            });
+        };
+        var successCallback = function (response) {
+            //Include the reference in the form
+            $form.append('<input type="hidden" class="korapay_referenceKey" name="korapay_referenceKey" value="' + response.reference + '"/>');
+
+            //Subnit the form to the payment gateway
+            $form.submit();
+
+           
+            $('body').block({
+                message: 'Processing Payment',
+                overlayCSS: {
+                    background: '#000',
+                    opacity: 0.8
+                },
+                css: {
+                    cursor: "wait"
+                }
+            });
+        };
+
+        //Initialize the transaction 
+        //Open korapay_modal
+        Korapay.initialize({
+            key: korapay_params.Key,
+            amount: total,
+            currency: 'NGN',
+            channels: ['card', 'bank_transfer'],
+            customerName: korapay_params.name,
+            customerEmail: korapay_params.email,
+            reference:korapay_params.reference,
+            onClose: function () {
+                //Remove the backdrop
+                $(this.el).unblock();
+
+            },
+            onSuccess: function (response) {
+                console.log(response)
+                successCallback(response)
+                return true
+            },
+            onFailed: function (response) {
+                console.log('error',response)
+                errorCallback(response)
+                return false
+            }
+        })
+     
+       return false
+    };
+
+
+
+    jQuery('#korapay-payment-button').click(function () {
+        return paymentRequest();
+    });
+});
